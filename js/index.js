e44ccc96d29bfe160ded1ba281080dcaf79a8ced
Luis Alberto Quesada Martinez <luisquesada163@gmail.com>
2022-08-21 19:25:49 -0300

    Base

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.querySelector('#boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)  
+}
+
+function seleccionarMascotaJugador() {
+    
+}
+
+window.addEventListener('load', iniciarJuego)
