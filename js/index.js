f4571cf1f549c6754f2dfb7efafd53298349447e
Luis Alberto Quesada Martinez <luisquesada163@gmail.com>
2022-10-08 16:10:31 -0300

    update

--- js/index.js
+++ js/index.js
@@ -40,7 +40,8 @@ let victoriasJugador = 0
 let victoriasEnemigo = 0
 let vidasJugador = 3
 let vidasEnemigo = 3
-let lienzo = mapa.getContext('2d')  /* Nos permite usar el lienzo en 2d para dibujar en el canavas */
+let lienzo = mapa.getContext('2d')  /* Usar lienzo en 2d para dibujar en el canavas */
+let intervalo
 
 class Mokepon {
     constructor(nombre, foto, vida) {
@@ -54,6 +55,8 @@ class Mokepon {
         this.alto = 80
         this.mapaFoto = new Image()
         this.mapaFoto.src = foto
+        this.velocidadX = 0
+        this.velocidadY = 0
     }
 }
 
@@ -119,6 +122,9 @@ function seleccionarMascotaJugador() {
 
     /* Canvas */
     sectionVerMapa.style.display = 'flex'
+
+    intervalo = setInterval(pintarPersonaje, 50)
+    
         
     
     if (inputHipodoge.checked) {
@@ -289,6 +295,8 @@ function numeroAleatorio(min, max) {
 }
 
 function pintarPersonaje() {
+    capipepo.x = capipepo.x + capipepo.velocidadX
+    capipepo.y = capipepo.y + capipepo.velocidadY
     lienzo.clearRect(0, 0, mapa.width, mapa.height)
     lienzo.drawImage(
         capipepo.mapaFoto,
@@ -299,9 +307,24 @@ function pintarPersonaje() {
     )
 }
 
-function moverCapipepo() {
-    capipepo.x = capipepo.x + 5
-    pintarPersonaje()
+function moverDerecha() {
+    capipepo.velocidadX = 5
+}
+
+function moverIzquierda() {
+    capipepo.x = velocidadX = -5
+}
+
+function moverAbajo() {
+    capipepo.y = velocidadY = 5
+}
+
+function moverArriba() {
+    capipepo.y = velocidadY = -5
+}
+
+function detenerMovimiento() {
+    
 }
 
 window.addEventListener('load', iniciarJuego)
